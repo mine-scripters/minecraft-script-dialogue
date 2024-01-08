091fd54ee92500a4e2a190ae07480da8b2f0a066
patriciamontalvo <montalvo.mar@gmail.com>
2024-01-08 13:27:19 -0700

    prettier

--- scripts/script_dialogue/MinecraftScriptDialogue/DualButtonScriptDialogue.test.ts
+++ scripts/script_dialogue/MinecraftScriptDialogue/DualButtonScriptDialogue.test.ts
@@ -22,7 +22,7 @@ describe('DualButtonScriptDialogue', () => {
   it('Without body', async () => {
     const player = mockPlayer();
 
-    await createDualButtonScriptDialogue().open({ player});
+    await createDualButtonScriptDialogue().open({ player });
 
     expect(MessageFormData).toHaveBeenCalledTimes(1);
 
